6919d959d7a09213f4b6a68844a5071739364bfd
Anton <prikhodkoanton4@gmail.com>
2026-04-20 20:01:26 +0200

    Main window change

--- ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
+++ ReactiveInteractiveUserInterface/GraphicalUserInterface/MainWindow.xaml.cs
@@ -13,25 +13,13 @@ using TP.ConcurrentProgramming.Presentation.ViewModel;
 
 namespace TP.ConcurrentProgramming.PresentationView
 {
-  /// <summary>
-  /// View implementation
-  /// </summary>
   public partial class MainWindow : Window
   {
     public MainWindow()
     {
-      Random random = new Random();
       InitializeComponent();
-      MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
-      double screenWidth = SystemParameters.PrimaryScreenWidth;
-      double screenHeight = SystemParameters.PrimaryScreenHeight;
-      viewModel.Start(random.Next(5, 10));
     }
 
-    /// <summary>
-    /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
-    /// </summary>
-    /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
     protected override void OnClosed(EventArgs e)
     {
       if (DataContext is MainWindowViewModel viewModel)
